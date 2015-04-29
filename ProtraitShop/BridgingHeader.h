d2de2fadd4d78e959cb01def7bff26308829244c
aochuih <aochuih@hotmail.com>
2015-04-29 09:41:10 +0800

    add BridgingHeader.h

---
+++ ProtraitShop/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  ProtraitShop
+//
+//  Created by aochuih on 15/4/29.
+//  Copyright (c) 2015年 NightWind. All rights reserved.
+//
+
+#ifndef ProtraitShop_BridgingHeader_h
+#define ProtraitShop_BridgingHeader_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+
+#endif
